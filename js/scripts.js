33ea2cbda7a15e84efbeb16f974598ef5715ad78
Christine Echo <christineecho@outlook.com>
2020-08-26 14:01:10 -0600

    Finishes JS code for API/AJAX fetch;

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 fetch ( 'http://numbersapi.com/random/year?json' )
     .then( response => response.json() )
-    .then( data => { console.log(data); } );
+    .then( data => { 
+
+        const factSpot = document.getElementById( 'fact-spot' );
+
+        // Grab the data we want to display
+        const newNumber = data.number;
+        const newFact = data.text;
+        
+        // Create the HTML elements
+        const factP = document.createElement( 'P' );
+        const factH2 = document.createElement( 'H2' );
+        const factPText = document.createElement( 'P' );
+
+        // Put the data into the elements
+        factH2.textContent = `${newNumber}`;
+        factPText.textContent = `${newFact}`;
+
+        // Append elements to parent paragraph
+        factP.appendChild( factH2 );
+        factP.appendChild( factPText );
+
+        // Add paragraph container into main HTML
+        factSpot.appendChild( factP );
+
+     } );
